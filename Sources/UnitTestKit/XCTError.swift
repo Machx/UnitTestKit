028fa75de268e8f25ad2f50f60165e50298f8945
Colin Wheeler <cocoasamurai@gmail.com>
2023-11-27 21:23:54 -0800

    add implementation
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Sources/UnitTestKit/XCTError.swift
+++ Sources/UnitTestKit/XCTError.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import XCTest
+
+public func XCTExpectError(_ file: StaticString = #file,
+						   _ line: UInt = #line,
+						   _ block: () throws -> Void) {
+	var errorThrown: Bool = false
+	do {
+		try block()
+	} catch {
+		errorThrown = true
+	}
+	guard errorThrown == false else { return }
+	XCTFail("Expected Error to be thrown in block but no error was thrown.\(file):\(line)",
+			file: file,
+			line: line)
+}
