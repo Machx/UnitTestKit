12baf6e02be3a8629ad4d5cc0f76f84147b8268f
Colin Wheeler <cocoasamurai@gmail.com>
2023-11-29 21:06:06 -0800

    test throw error
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Tests/UnitTestKitTests/XCTExpectErrorTests.swift
+++ Tests/UnitTestKitTests/XCTExpectErrorTests.swift
@@ -6,24 +6,21 @@
 //
 
 import XCTest
+import UnitTestKit
 
 final class XCTExpectErrorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+	enum ExpectError: Error {
+		case exampleError
+	}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+	func testExpectThrowError() throws {
+		XCTExpectFailure("Code block does not throw which should trigger XCTExpectError to fail.")
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+		XCTExpectError {
+			print("Hello")
+		}
+	}
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
