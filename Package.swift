843b6c9a9fa60693aa0f54122f76680489ca765f
Colin Wheeler <cocoasamurai@gmail.com>
2024-05-06 21:36:45 -0700

    add konkyo as a dependency
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Package.swift
+++ Package.swift
@@ -11,11 +11,17 @@ let package = Package(
             name: "UnitTestKit",
             targets: ["UnitTestKit"]),
     ],
+	dependencies: [
+		.package(url: "https://github.com/Machx/Konkyo.git", branch: "main"),
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "UnitTestKit",
+			dependencies: [
+				.product(name: "Konkyo", package: "Konkyo"),
+			],
 			linkerSettings: [
 				.linkedFramework("XCTest")
 			]
