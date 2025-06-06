1c2415bc0d85138e8736e64a791a6e94b3ad1a60
Colin Wheeler <cocoasamurai@gmail.com>
2025-06-06 11:12:20 -0700

    add tests
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Tests/UnitTestKitTests/UTKExpectErrorXCTTests.swift
+++ Tests/UnitTestKitTests/UTKExpectErrorXCTTests.swift
@@ -5,12 +5,41 @@
 //  Created by Colin Wheeler on 6/5/25.
 //
 
+import XCTest
+import UnitTestKit
 import Testing
 
-struct UTKExpectErrorXCTTests {
+final class UTKExpectErrorXCTTests: XCTestCase {
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+	enum ExpectError: Error {
+		case exampleError
+	}
 
+	func testExpectThrowError() throws {
+		XCTExpectFailure("Code block does not throw which should trigger UTKExpectError to fail.")
+
+		UTKExpectErrorXCT {
+			print("Hello World!")
+		}
+	}
+
+	func testExpectErrorThrows() throws {
+		UTKExpectError {
+			throw ExpectError.exampleError
+		}
+	}
+
+	func testExpectNoError() throws {
+		XCTExpectFailure("Code block throws an error, which should trigger UTKExpectNoError to fail.")
+
+		UTKExpectNoErrorXCT {
+			throw ExpectError.exampleError
+		}
+	}
+
+	func testExpectNoErrorNoThrows() throws {
+		UTKExpectNoErrorXCT {
+			print("Hello World!")
+		}
+	}
 }
