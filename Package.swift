352af75c6ee56defaf484f69b6f91066b8054c7d
Colin Wheeler <cocoasamurai@gmail.com>
2025-06-18 00:00:06 -0700

    stub
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
         .target(
             name: "UnitTestKit",
 			dependencies: [
+				//.product(name: "", package: "")
 			],
 			linkerSettings: [
 				.linkedFramework("XCTest")
